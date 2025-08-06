ae66e0dec82f5754a2691b580e8ff91ea354e72b
latha-414 <144301489+latha-414@users.noreply.github.com>
2025-08-06 10:41:41 +0530

    Update variables.tf

--- api-gateway/variables.tf
+++ api-gateway/variables.tf
@@ -1,19 +1,5 @@
 # variables.tf
-variable "client_id" {
-  type = string
-}
 
-variable "client_secret" {
-  type = string
-}
-
-variable "tenant_id" {
-  type = string
-}
-
-variable "subscription_id" {
-  type = string
-}
 
 variable "location" {
   type    = string
